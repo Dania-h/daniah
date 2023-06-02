7c1a3863e8e0cf43097a0c25325dda8428b9bde3
Dania <dabeaher@yahoo.com>
2023-06-02 02:36:22 -0400

    Initializing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+            About
+        </li>
+        <li>
+            Skills
+        </li>
+        <li>
+            Projects
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import "./Hero.scss";
+
+function Hero() {
+  return (
+    <div>
+      <h1>DANIA HERNANDEZ</h1>
+    </div>
+  );
+}
+
+export default Hero;
