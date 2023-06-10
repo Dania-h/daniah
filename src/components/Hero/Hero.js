6ed5bd4fadf5af65e3a236085fd8179786afc8ed
Dania <dabeaher@yahoo.com>
2023-06-09 20:05:22 -0400

    Add arrow animation and finish hero basic structure

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,18 @@
 import "./Hero.scss";
-import portrait from "../../assets/images/IMG_9986-small.jpg";
+import portrait from "../../assets/images/IMG_9986-bw.png";
+import scroll from "../../assets/icons/scroll-down.svg";
 
 function Hero() {
   return (
     <article className="hero">
       <div className="hero__div">
         <h1 className="hero__name">DANIA HERNANDEZ</h1>
-        <p>Welcome! I'm a full stack developer who loves being curious, creative, and detailed in everything I do.</p>
-        <img
-          className="hero__portrait"
-          src={portrait}
-          alt="portrait of Dania"
-        />
+        <h2 className="hero__subheader">FULL STACK SOFTWARE ENGINEER</h2>
+        <p className="hero__text">Curious, creative, detailed</p>
+      </div>
+      <img className="hero__portrait" src={portrait} alt="portrait of Dania" />
+      <div className="hero__scroll">
+        <img className="hero__scroll--arrow" src={scroll} alt="scroll down" />
       </div>
     </article>
   );
