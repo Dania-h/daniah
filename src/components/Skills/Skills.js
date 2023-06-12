427ce437a5d0dea09a5e476bde7986f106a94229
Dania <dabeaher@yahoo.com>
2023-06-12 18:19:24 -0400

    Create data folder and file with skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,56 +1,7 @@
 import "./Skills.scss";
-import reactLogo from "../../assets/icons/react-2.svg";
-import html from "../../assets/icons/w3_html5-icon.svg";
-import css from "../../assets/icons/icons8-css-logo.svg";
-import sass from "../../assets/icons/sass-1.svg";
-import javascript from "../../assets/icons/logo-javascript.svg";
-import node from "../../assets/icons/nodejs-icon.svg";
-import git from "../../assets/icons/Git-Icon-1788C.svg";
-import express from "../../assets/icons/expressjs-icon.svg";
+import skills from "../../data/skills";
 
 function Skills() {
-  const skills = [
-    {
-      id: "1",
-      text: "React",
-      logo: reactLogo,
-    },
-    {
-      id: "2",
-      text: "HTML",
-      logo: html,
-    },
-    {
-      id: "3",
-      text: "CSS",
-      logo: css,
-    },
-    {
-      id: "4",
-      text: "SASS",
-      logo: sass,
-    },
-    {
-      id: "5",
-      text: "Javascript",
-      logo: javascript,
-    },
-    {
-      id: "6",
-      text: "Node.js",
-      logo: node,
-    },
-    {
-      id: "7",
-      text: "Git",
-      logo: git,
-    },
-    {
-      id: "8",
-      text: "Express.js",
-      logo: express,
-    },
-  ];
 
   return (
     <section id="skills" className="skills">
---
+++ src/data/skills.js
@@ -0,0 +1,53 @@
+import reactLogo from "../assets/icons/react-2.svg";
+import html from "../assets/icons/w3_html5-icon.svg";
+import css from "../assets/icons/icons8-css-logo.svg";
+import sass from "../assets/icons/sass-1.svg";
+import javascript from "../assets/icons/logo-javascript.svg";
+import node from "../assets/icons/nodejs-icon.svg";
+import git from "../assets/icons/Git-Icon-1788C.svg";
+import express from "../assets/icons/expressjs-icon.svg";
+
+const skills = [
+  {
+    id: "1",
+    text: "React",
+    logo: reactLogo,
+  },
+  {
+    id: "2",
+    text: "HTML",
+    logo: html,
+  },
+  {
+    id: "3",
+    text: "CSS",
+    logo: css,
+  },
+  {
+    id: "4",
+    text: "SASS",
+    logo: sass,
+  },
+  {
+    id: "5",
+    text: "Javascript",
+    logo: javascript,
+  },
+  {
+    id: "6",
+    text: "Node.js",
+    logo: node,
+  },
+  {
+    id: "7",
+    text: "Git",
+    logo: git,
+  },
+  {
+    id: "8",
+    text: "Express.js",
+    logo: express,
+  },
+];
+
+export default skills
