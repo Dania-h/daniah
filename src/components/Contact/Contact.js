ea86e8940e81d96724f9d10f036d921ba9905be7
Dania <dabeaher@yahoo.com>
2023-06-19 18:38:15 -0400

    Style the contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,10 +1,10 @@
 import "./Contact.scss";
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function Contact() {
   const form = useRef();
-  
+
   const SERVICE_ID = process.env.REACT_APP_YOUR_SERVICE_ID;
   const TEMPLATE_ID = process.env.REACT_APP_YOUR_TEMPLATE_ID;
   const PUBLIC_KEY = process.env.REACT_APP_YOUR_PUBLIC_KEY;
@@ -32,15 +32,20 @@ function Contact() {
 
   return (
     <section className="contact">
-      <h2 className="contact__header">CONTACT ME</h2>
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+      <div className="contact__text">
+        <h2 className="contact__header">CONTACT ME</h2>
+        <p>
+          I love hearing about all sorts of projects and from all sorts of people! Don't hesitate to reach out.
+        </p>
+      </div>
+      <form className="contact__form" ref={form} onSubmit={sendEmail}>
+        <label className="contact__form--label">Name</label>
+        <input className="contact__form--input" type="text" name="user_name" required />
+        <label className="contact__form--label">Email</label>
+        <input className="contact__form--input" type="email" name="user_email" required />
+        <label className="contact__form--label">Message</label>
+        <textarea className="contact__form--input" name="message" required />
+        <input className="contact__form--submit" type="submit" value="SEND" />
       </form>
     </section>
   );
