972c886358faf4292958732b8645df90b1b3aa71
Dania <dabeaher@yahoo.com>
2023-06-28 13:24:38 -0400

    Create responsive hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,11 +7,21 @@ function Hero() {
   return (
     <article className="hero">
       <div className="hero__div">
-        <h1 className="hero__name">DANIA HERNANDEZ</h1>
-        <h3 className="hero__subheader">SOFTWARE ENGINEER</h3>
-        <p className="hero__text">I'm a curious, creative, and detailed software developer from Miami. When it comes to building applications, you can find me at the intersection of logical thinking and clean design.</p>
+        <div className="hero__details">
+          <h1 className="hero__name">DANIA HERNANDEZ</h1>
+          <h3 className="hero__subheader">SOFTWARE ENGINEER</h3>
+          <p className="hero__text">
+            I'm a curious, creative, and detailed software developer from Miami.
+            When it comes to building applications, you can find me at the
+            intersection of logical thinking and clean design.
+          </p>
+        </div>
+        <img
+          className="hero__portrait"
+          src={portrait}
+          alt="portrait of Dania"
+        />
       </div>
-      <img className="hero__portrait" src={portrait} alt="portrait of Dania" />
       <HashLink smooth to="/#skills">
         <div className="hero__scroll">
           <img className="hero__scroll--arrow" src={scroll} alt="scroll down" />
